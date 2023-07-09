5eb0bed2a81283722a3485f06d85d896d6eb98bc
mogoatlhe <iam@ofent.se>
2023-07-09 16:48:48 +0200

    convert array to BST and return root

--- src/models/tree.js
+++ src/models/tree.js
@@ -5,9 +5,38 @@ const Tree = (array) => {
     if (a < b) return -1;
     return 1;
   });
-  let root;
+  let root = buildTree(0, arr.length - 1);
 
-  return {};
+  function buildTree(start, end) {
+    if (end < start) return null;
+    const mid = parseInt((start + end) / 2);
+    let node = _getNewNode(arr[mid]);
+    node.left = buildTree(start, mid - 1);
+    node.right = buildTree(mid + 1, end);
+
+    return node;
+  }
+
+  const prettyPrint = (node = root, prefix = "", isLeft = true) => {
+    if (node === null) {
+      return;
+    }
+    if (node.right !== null) {
+      prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+    }
+    console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.getData()}`);
+    if (node.left !== null) {
+      prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+    }
+  };
+
+  function _getNewNode(value) {
+    const node = Node();
+    node.setData(value);
+    return node;
+  }
+
+  return { prettyPrint };
 };
 
 export default Tree;
